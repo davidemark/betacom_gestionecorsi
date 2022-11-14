619d4c5a3a3c063b3401a519107d2c3af535107b
Betacom <Betacom@DESKTOP-H3D7VP0>
2022-11-14 18:14:31 +0100

    Docente.java

--- GestioneCorsi/src/main/java/com/torino/gestionecorsi/businesscomponent/model/Docente.java
+++ GestioneCorsi/src/main/java/com/torino/gestionecorsi/businesscomponent/model/Docente.java
@@ -1,5 +1,34 @@
 package com.torino.gestionecorsi.businesscomponent.model;
 
 public class Docente {
-
+	private String nome;
+	private String cognome;
+	private String cvdocente;
+	private long coddcocente;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getCvdocente() {
+		return cvdocente;
+	}
+	public void setCvdocente(String cvdocente) {
+		this.cvdocente = cvdocente;
+	}
+	public long getCoddcocente() {
+		return coddcocente;
+	}
+	public void setCoddcocente(long coddcocente) {
+		this.coddcocente = coddcocente;
+	}
+	
 }
