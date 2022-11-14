1261f2fdc19ade2a978984c90e8d4d38c0306281
Betacom <Betacom@DESKTOP-EJFI9HQ>
2022-11-14 14:33:49 +0100

    creazione tabelle nel DB e configurazione file properties

---
+++ GestioneCorsi/SQL/create_obj.sql
@@ -0,0 +1,39 @@
+create table Amministratore(
+codadmin int,
+nomeadmin varchar2(30) not null,
+cognome varchar2(30) not null,
+constraint p_codadmin primary key (codadmin));
+
+create table Corso(
+codcorso int,
+nomecorso varchar2(30) not null,
+datainiziocorso Date not null,
+datafinecorso Date not null,
+corsocosto number (6,2) not null,
+commenticorso varchar2(300) not null,
+aulacorso varchar2(3) not null,
+constraint p_codcorso primary key (codcorso));
+
+create table Corsista(
+codcorsista int,
+nomecorsista varchar2(30) not null,
+cognomecorsista Date not null,
+precedentiformativi number (1) not null,
+constraint p_codcorsista primary key (codcorsista));
+
+create table Docente(
+coddocente int,
+nomedocente varchar2(30) not null,
+cognomedocente varchar2(30) not null,
+cvdocente varchar2(100) not null,
+constraint p_coddocente primary key (coddocente));
+
+create table corso_corsista(
+codcorso int,
+codcorsista int,
+constraint f_codcorso foreign key(codcorso) references corso(codcorso) on delete cascade,
+constraint f_codcorsista foreign key(codcorsista) references corsista(codcorsista) on delete cascade,
+constraint p_cc primary key(codcorso, codcorsista));
+
+-- Sequenze
+create sequence corsista_seq;
---
+++ GestioneCorsi/SQL/delete_obj.sql
@@ -0,0 +1,6 @@
+drop table amministratore cascade constraints;
+drop table corso cascade constraints;
+drop table corsista cascade constraints;
+drop table docente cascade constraints;
+drop table corso_corsista cascade constraints;
+drop sequence corsista_seq;
