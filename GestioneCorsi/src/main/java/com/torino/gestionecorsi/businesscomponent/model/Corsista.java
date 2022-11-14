41474253cc6c73a80caee8fa9255fd391c247d57
Betacom <Betacom@DESKTOP-IC9Q9FK.station>
2022-11-14 17:33:25 +0100

    Corsista.java

--- GestioneCorsi/src/main/java/com/torino/gestionecorsi/businesscomponent/model/Corsista.java
+++ GestioneCorsi/src/main/java/com/torino/gestionecorsi/businesscomponent/model/Corsista.java
@@ -1,5 +1,35 @@
 package com.torino.gestionecorsi.businesscomponent.model;
 
 public class Corsista {
-
+	private String nome;
+	private String cognome;
+	private String precedentiformativi;
+	private String codcorsista;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getPrecedentiformativi() {
+		return precedentiformativi;
+	}
+	public void setPrecedentiformativi(String precedentiformativi) {
+		this.precedentiformativi = precedentiformativi;
+	}
+	public String getCodcorsista() {
+		return codcorsista;
+	}
+	public void setCodcorsista(String codcorsista) {
+		this.codcorsista = codcorsista;
+	}
+	
+	
+	
 }
