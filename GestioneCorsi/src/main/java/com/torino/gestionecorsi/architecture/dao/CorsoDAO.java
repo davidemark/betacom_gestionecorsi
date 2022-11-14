5160ee7cd8b24b5f8f357fca49a30efb8d680c60
Betacom <Betacom@DESKTOP-EJFI9HQ>
2022-11-14 18:08:34 +0100

    implementazione model corso e CorsoDAO

--- GestioneCorsi/src/main/java/com/torino/gestionecorsi/architecture/dao/CorsoDAO.java
+++ GestioneCorsi/src/main/java/com/torino/gestionecorsi/architecture/dao/CorsoDAO.java
@@ -1,5 +1,51 @@
 package com.torino.gestionecorsi.architecture.dao;
 
-public class CorsoDAO {
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.rowset.CachedRowSet;
+import javax.sql.rowset.RowSetProvider;
+
+import com.torino.gestionecorsi.businesscomponent.model.Corso;
+
+public class CorsoDAO implements GenericDAO<Corso>, DAOConstants {
+
+	private CachedRowSet rowSet;
+	
+	public static CorsoDAO getFactory() throws DAOException {
+		return new CorsoDAO();
+	}
+
+	private CorsoDAO() throws DAOException {
+		try {
+			rowSet = RowSetProvider.newFactory().createCachedRowSet();
+		} catch (SQLException sql) {
+			throw new DAOException(sql);
+		}
+	}
+
+	@Override
+	public void create(Connection conn, Corso entity) throws DAOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(Connection conn, Corso entity) throws DAOException {
+		
+
+	}
+
+	@Override
+	public Corso getByCod(Connection conn, long id) throws DAOException {
+		
+		return null;
+	}
+
+	@Override
+	public Corso[] getAll(Connection conn) throws DAOException {
+		
+		return null;
+	}
 
 }
--- GestioneCorsi/src/main/java/com/torino/gestionecorsi/businesscomponent/model/Corso.java
+++ GestioneCorsi/src/main/java/com/torino/gestionecorsi/businesscomponent/model/Corso.java
@@ -1,5 +1,76 @@
 package com.torino.gestionecorsi.businesscomponent.model;
 
-public class Corso {
+import java.util.Date;
 
+public class Corso {
+	private long codcorso;
+	private String nome;
+	private Date dataInizio;
+	private Date dataFine;
+	private Double costo;
+	private String commento;
+	
+	public long getCodcorso() {
+		return codcorso;
+	}
+	
+	public void setCodcorso(long codcorso) {
+		this.codcorso = codcorso;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public Date getDataInizio() {
+		return dataInizio;
+	}
+	
+	public void setDataInizio(Date dataInizio) {
+		this.dataInizio = dataInizio;
+	}
+	
+	public Date getDataFine() {
+		return dataFine;
+	}
+	
+	public void setDataFine(Date dataFine) {
+		this.dataFine = dataFine;
+	}
+	
+	public Double getCosto() {
+		return costo;
+	}
+	
+	public void setCosto(Double costo) {
+		this.costo = costo;
+	}
+	
+	public String getCommento() {
+		return commento;
+	}
+	
+	public void setCommento(String commento) {
+		this.commento = commento;
+	}
+	public String getAula() {
+		return aula;
+	}
+	
+	public void setAula(String aula) {
+		this.aula = aula;
+	}
+	
+	private String aula;
+
+	@Override
+	public String toString() {
+		return "Corso [codcorso=" + codcorso + ", nome=" + nome + ", dataInizio=" + dataInizio + ", dataFine="
+				+ dataFine + ", costo=" + costo + ", commento=" + commento + ", aula=" + aula + "]";
+	}
+	
 }
