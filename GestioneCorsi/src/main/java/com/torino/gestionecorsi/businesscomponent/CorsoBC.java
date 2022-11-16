332fd9fdf996605ce56356d5c68d1ef6566d08a9
Betacom <Betacom@DESKTOP-EJFI9HQ>
2022-11-16 09:50:50 +0100

    creazione pacchetto test JUnit per i BC

---
+++ GestioneCorsi/src/main/java/com/torino/gestionecorsi/businesscomponent/CorsoBC.java
@@ -0,0 +1,29 @@
+package com.torino.gestionecorsi.businesscomponent;
+
+import java.io.IOException;
+import java.sql.Connection;
+
+import com.torino.gestionecorsi.architecture.dao.CorsoDAO;
+import com.torino.gestionecorsi.architecture.dao.DAOException;
+import com.torino.gestionecorsi.architecture.dbaccess.DBAccess;
+import com.torino.gestionecorsi.businesscomponent.model.Corso;
+
+public class CorsoBC {
+	private Connection conn;
+
+	public CorsoBC() throws ClassNotFoundException, DAOException, IOException {
+		conn = DBAccess.getConnection();
+	}
+
+	public Corso findByCod(long cod) throws DAOException {
+		return CorsoDAO.getFactory().getByCod(conn, cod);
+	}
+
+	public Corso[] getArticoli() throws DAOException {
+		return CorsoDAO.getFactory().getAll(conn);
+	}
+
+	public void delete(Corso corso) throws DAOException {
+		CorsoDAO.getFactory().delete(conn, corso);
+	}
+}
