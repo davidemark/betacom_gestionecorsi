0e8fb2ba0c1b4e5fcfa2f77f7ac9142a19d8bcac
Betacom <Betacom@DESKTOP-EJFI9HQ>
2022-11-18 14:21:18 +0100

    sistemazione tabelle

--- GestioneCorsi/SQL/insert_table.sql
+++ GestioneCorsi/SQL/insert_table.sql
@@ -11,16 +11,16 @@ insert into docente values(9, 'Pippo', 'Baudo', 'SPRING');
 insert into docente values(10, 'Giancarlo', 'Magalli', 'JAVA, PHP, XML, CSS, HTML, JS, SPRING, C, C++, PHYTON');
 
 -- corso
-insert into corso values(1, 'JAVA', '12-mag-2020', '15-mag-2021', 1700, 'molto bello', 'a1',1);
-insert into corso values(2, 'PHP', '18-dic-2020', '12-gen-2021', 700, 'poco utile', 'a2',1);
-insert into corso values(3, 'XML', '3-dic-2020', '13-feb-2021', 500, 'utile', 'a3',3);
-insert into corso values(4, 'CSS', '23-set-2020', '16-dic-2020', 800, 'interessante', 'a4',5);
-insert into corso values(5, 'HTML', '5-ott-2020', '14-nov-2020', 450, 'fondamentale', 'a5',7);
-insert into corso values(6, 'JS', '10-lug-2020', '17-lug-2020', 2000, 'molto utile', 'a6',9);
-insert into corso values(7, 'SPRING', '7-mag-2020', '18-mag-2020', 1800, 'istruttivo', 'a7',8);
-insert into corso values(8, 'C', '21-nov-2020', '19-dic-2020', 999, 'formativo', 'a8',6);
-insert into corso values(9, 'PYTHON', '11-nov-2020', '10-mar-2021', 1500, 'troppo mainstream', 'a9',4);
-insert into corso values(10, 'C++', '8-gen-2020', '15-feb-2020', 1099, 'formativo++', 'a10',2);
+insert into corso values(1, 'JAVA', '19-dic-2022', '15-nov-2023', 1700, 'molto bello', 'a1', 1); -- corso che deve iniziare
+insert into corso values(2, 'PHP', '20-gen-2023', '12-feb-2023', 700, 'poco utile', 'a2', 1); -- corso che deve iniziare
+insert into corso values(3, 'XML', '15-mar-2023', '13-mag-2023', 500, 'utile', 'a3', 3); -- corso che deve iniziare
+insert into corso values(4, 'CSS', '23-set-2023', '16-dic-2023', 800, 'interessante', 'a4', 5); -- corso che deve iniziare
+insert into corso values(5, 'HTML', '5-ott-2023', '14-nov-2023', 450, 'fondamentale', 'a5', 7); -- corso che deve iniziare
+insert into corso values(6, 'JS', '17-nov-2022', ' 17-feb-2023', 2000, 'molto utile', 'a6', 9); -- corso in corso
+insert into corso values(7, 'SPRING', '7-nov-2022', '18-gen-2023', 1800, 'istruttivo', 'a7', 8); -- corso in corso
+insert into corso values(8, 'C', '5-ott-2022', '17-nov-2022', 999, 'formativo', 'a8', 6); -- corso scaduto
+insert into corso values(9, 'PYTHON', '11-nov-2022', '16-nov-2022', 1500, 'troppo mainstream', 'a9', 4); -- corso scaduto
+insert into corso values(10, 'C++', '15-dic-2025', '15-dic-2025', 1099, 'formativo++', 'a10', 2); -- corso non valido
 
 -- corsista
 insert into corsista values(corsista_seq.nextval, 'Lucio', 'Mastropietro', 1);
